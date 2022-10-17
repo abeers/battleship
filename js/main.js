78bc6ca6f4282744fa2009d9b74d8027d49fce49
Alex Beers <alexbeers92@gmail.com>
2022-10-17 17:58:06 -0400

    Modify start game functionality

--- js/main.js
+++ js/main.js
@@ -4,17 +4,39 @@ const COLUMNS = 10
 const AI_NAME = 'Computer'
 
 /*----- state variables -----*/
-
+let isPlacing = true
+let mainPlayer
 
 /*----- cached elements  -----*/
-
+const mainBoard = document.querySelector('#main-board')
+const newGameButton = document.querySelector('#new-game')
 
 /*----- event listeners -----*/
-
+newGameButton.addEventListener('click', () => new Game('Alex').startGame())
 
 /*----- functions -----*/
+class Game {
+  constructor (playerName) {
+    this.players = [
+      new Player(playerName, false),
+      new Player()
+    ]
+    this.currentPlayer = 0
+  }
+
+  startGame = () => {
+    this.players.forEach(player => {
+      player.board.createBoard()
+    })
+    
+    mainPlayer = this.players[0]
+    mainPlayer.board.displayBoard(mainBoard)
+    console.log(this)
+  }
+}
+
 class Player {
-  constructor (name = AI_NAME) {
+  constructor (name = AI_NAME, isAI = true) {
     this.name = name
     this.board = new Board()
     this.ships = [
@@ -24,25 +46,62 @@ class Player {
       new Ship('Submarine', 3),
       new Ship('Destroyer', 2)
     ]
+    this.isAI = isAI
   }
 
   hasLost = () => this.ships.every(ship => ship.isSunk)
 }
 
 class Board {
-  constructor () {
-    this.cells = this.createBoard()
+  constructor() {
+    this.cells = []
   }
 
   createBoard = (rows = ROWS, columns = COLUMNS) => {
     const board = []
+
     for (let i = 0; i < rows; i++) {
       board.push([])
+
       for (let j = 0; j < columns; j++) {
-        board[i].push(new Space())
+        const position = j + i * rows
+        board[i].push(new Space(position))
       }
     }
-    return board
+
+    this.cells = board
+  }
+
+  displayBoard(boardEl) {
+    boardEl.innerHTML = ''
+    
+    let templateRows = ''
+    let templateColumns = ''
+
+    for (let i = 0; i < this.cells.length; i++) {
+      templateRows += '1fr '
+
+      for (let j = 0; j < this.cells[i].length; j++) {
+        if (j === 0) {
+          templateColumns += '1fr '
+        }
+
+        const newBoardSpace = document.createElement('div')
+        const position = j + i * this.cells.length
+
+        newBoardSpace.classList.add('board-space')
+        newBoardSpace.dataset.position = position
+
+        boardEl.appendChild(newBoardSpace)
+      }
+    }
+
+    boardEl.style.gridTemplateRows = templateRows
+    boardEl.style.gridTemplateColumns = templateColumns
+
+    boardEl.addEventListener('click', (event) =>
+      console.log(event.target.dataset.position)
+    )
   }
 }
 
@@ -57,11 +116,26 @@ class Ship {
   constructor (name, size) {
     this.name = name
     this.size = size
+
+    this.hits = 0
     this.isSunk = false
+
     this.startSpace = null
-    this.orientation = 'right'
+    this.isHorizontal = true
+  }
+
+  rotateShip () {
+    this.isHorizontal = !this.isHorizontal
+  }
+
+  placeShip (space) {
+    this.startSpace = space
+  }
+
+  hitShip () {
+    this.hits++
+    if (this.hits >= this.size) {
+      this.isSunk = true
+    }
   }
 }
-
-const me = new Player('Alex')
-console.log(me.hasLost())
